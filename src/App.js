25d2a6c99a1933e4910e67b35bd2930de10c96e2
Derek DeHart <derek.dehart@gmail.com>
2020-11-11 20:24:00 -0500

    Refactor List to accept list prop and move stories data into App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = event => {
     console.log(event)
   };
@@ -33,13 +33,13 @@ const App = () => {
 
       <hr/>
 
-      <List/>
+      <List list={stories}/>
     </div>
   );
 }
 
-const List = () =>
-  stories.map(story => (
+const List = props =>
+  props.list.map(story => (
     <div key={story.objectID}>
       <span>
         <a href={story.url}>{story.title}</a>

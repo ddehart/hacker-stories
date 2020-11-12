a78e3164571e985c1a4d9bfd339b41177b3bd471
Derek DeHart <derek.dehart@gmail.com>
2020-11-11 21:59:56 -0500

    Add separate Search component to contain search functionality

--- src/App.js
+++ src/App.js
@@ -20,6 +20,20 @@ const App = () => {
     },
   ];
 
+  return (
+    <div>
+      <h1>Hacker Stories</h1>
+
+      <Search />
+
+      <hr/>
+
+      <List list={stories}/>
+    </div>
+  );
+};
+
+const Search = () => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = event => {
@@ -28,21 +42,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>Hacker Stories</h1>
-
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' onChange={handleChange}/>
 
       <p data-testid='searching-for'>
         Searching for <strong>{searchTerm}</strong>.
       </p>
-
-      <hr/>
-
-      <List list={stories}/>
     </div>
   );
-}
+};
 
 const List = props =>
   props.list.map(story => (
